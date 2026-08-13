5d9da78e14549069ab6ac75827329b4abe62866a
HarshRawal-CWW <harsh.rawal@ipg-teamdynamic.com>
2026-08-13 19:08:02 +0530

    chore: upgrade Sanity dependencies to v6.9.2 and configure studio host and app ID

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -5,7 +5,9 @@ export default defineCliConfig({
     projectId: 'sn0k73pc',
     dataset: 'production'
   },
+  studioHost: 'rj-portfolio',
   deployment: {
+    appId: 'ev7c2o0hp8eaqa85gprd2sqd',
     /**
      * Enable auto-updates for studios.
      * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
